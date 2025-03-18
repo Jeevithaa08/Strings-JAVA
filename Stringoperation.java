951c45c6ef2eafebc47e7ded5957a4c7aeda8615
Jeevithaa08 <jeevitha08102005@gmail.com>
2025-03-18 20:01:28 +0530

    Add files via upload

---
+++ Stringoperation.java
@@ -0,0 +1,10 @@
+package Project2;
+
+public class Stringoperation {
+public static void main(String[] args)
+{
+	String name = "ram";
+
+	System.out.println(name);
+}
+}
---
+++ Strings.java
@@ -0,0 +1,114 @@
+package Project2;
+import java.util.*;
+public class Strings {
+	public static void main(String[] args) {
+		/*String str="hello everyone";
+		 * 
+		System.out.println(str.length()); //length of the string
+		
+		System.out.println(str.substring(4)); //strating of the string
+		
+		System.out.println(str.substring(4,7)); //Starting and ending of the string
+		
+		System.out.println(str.indexOf("Hel")); //IndexOF
+		
+		System.out.println(str.indexOf("yon"));*/
+		
+		
+		//case Changing
+		/*String str="Java Programming";
+		System.out.println(str.toUpperCase());
+		System.out.println(str.toLowerCase());*/
+		
+		//replaceing Charcters
+		/*String str="Java Programming";
+		System.out.println(str.replace('a','A'));*/
+		
+		//Equal 
+		/*String str="JAVA";
+		String str1 = "java";
+		System.out.println(str==str1);
+		System.out.println(str.equals(str1));
+		
+		//ignoring cases
+		System.out.println(str.equalsIgnoreCase(str1));	*/	
+		
+		//charAt
+		/*String str="JAVA";
+		System.out.println(str.charAt(2));*/ //getting the specific character
+	
+		/*// Split
+		  
+		String str="java,python,springboot";
+		String []languarge = str.split(",");
+		
+		for(String s1 :language) {
+			System.out.println(s1);
+		}*/
+		
+		//Concatination
+		/*String str="JAVA";
+		String str1 = "java";
+		System.out.println("Concat String:"+str.concat(str1));*/
+		
+		//Trim
+		/*String str="  JAVA   ";
+		System.out.println(str.length());
+		String str1 = str.trim();
+		System.out.println(str1);
+		System.out.println(str1.length());*/
+		
+		//CompareTo
+		/*String str ="java";
+		String str1 = "java";
+		
+        System.out.println(str.compareTo(str1));*/
+		
+		//Contains
+		
+		/*String str ="java";
+		System.out.println(str.contains("java"));
+		System.out.println(str.contains("python"));*/
+
+		//intern
+		/*String s1 = new String("JavaDeveloper");
+		String s2 = "JavaDeveloper";
+		String s3 = s1.intern();
+		System.out.println(s1==s2);
+		System.out.println(s2==s3);*/
+		
+		//StringBuilder 
+		/*StringBuilder sb = new StringBuilder("Hello");
+		//Append
+		sb.append("  World");
+		System.out.println("After append: " +sb);
+		sb.insert(5, ",");
+		System.out.println("After Insert: " + sb);
+		
+		//Replace
+		sb.replace(6, 11, "Java");
+		System.out.println("After replace: "+ sb);
+		//Reverse
+		sb.reverse();
+		System.out.println("After reverse: " + sb);*/
+		
+		//StringBuffer
+		/*StringBuffer sb = new StringBuffer("Hello");
+		//Append
+		sb.append("  World");
+		System.out.println("After append: " +sb);
+		sb.insert(5, ",");
+		System.out.println("After Insert: " + sb);
+		
+		//Replace
+		sb.replace(6, 11, "Java");
+		System.out.println("After replace: "+ sb);
+		//Reverse
+		sb.reverse();
+		System.out.println("After reverse: " + sb);*/
+		
+		
+		
+}
+}
+
