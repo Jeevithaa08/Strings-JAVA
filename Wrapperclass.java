af6b9bc0760b019283f820e43bea245a4a037fd5
Jeevithaa08 <jeevitha08102005@gmail.com>
2025-03-18 20:04:48 +0530

    Add files via upload

---
+++ Wrapperclass.java
@@ -0,0 +1,14 @@
+package Project2;
+import java.util.*;
+public class Wrapperclass {
+	public static void main(String[] args) {
+		
+		//Autoboxing : convert primitive to wrapper
+		Integer intObj = 50;
+		
+		//Unboxing : convert wrapper to primitive
+		int intValue = intObj;
+		System.out.println(intValue);
+	}
+
+}
