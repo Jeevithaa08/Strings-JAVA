b767542b7b7ad26fcd767ca7434628d54fd7c339
Jeevithaa08 <jeevitha08102005@gmail.com>
2025-03-18 20:05:30 +0530

    Add files via upload

---
+++ Userinput.java
@@ -0,0 +1,10 @@
+package Project2;
+import java.util.*;
+public class Userinput {
+	public static void main(String[] args) {
+	Scanner s1 = new Scanner(System.in);
+	System.out.print("enter a name:");
+	String name=s1.nextLine();
+	System.out.println(name);
+	}
+}
